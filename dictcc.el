7588c09302e391b95264452bbecf2462ae41aa46
Marten Lienen <marten.lienen@gmail.com>
2015-07-14 00:36:46 +0200

    Use GPLv3

--- dictcc.el
+++ dictcc.el
@@ -7,6 +7,21 @@
 ;; Keywords: convenience
 ;; Package-Requires: ((s "1.0") (dash "2.0") (helm))
 
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; Look up translations on dict.cc. You then pick one of them through a helm
